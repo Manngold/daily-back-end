809d47eb8e47b89814391c8a4d8a504496081d0a
Manngold <jsw4820@gmail.com>
2019-01-30 20:19:18 +0900

    basic setting socket.io

--- socket-chat/index.js
+++ socket-chat/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import http from "http";
+import io from "socket.io";
 
 const app = express();
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log("server is running");
+http.Server(app);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:3000");
 });
