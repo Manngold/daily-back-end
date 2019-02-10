7b406ccadf5e63eba50d083490f179d0c8dec5b3
Manngold <jsw4820@gmail.com>
2019-02-10 21:16:44 +0900

    find my location

--- daum-map-api/app.js
+++ daum-map-api/app.js
@@ -3,20 +3,9 @@ import latitude from "latitude";
 
 const app = express();
 const PORT = 3000;
-const APP_KEY = "6651e6add2bfc2a9c421519fbd190944";
-let lat, lng;
 
-const financial = unfixed => Number.parseFloat(unfixed).toFixed(5);
-
-latitude().then(location => {
-  lat = financial(location.latitude);
-  lng = financial(location.longitude);
-});
-
-app.set("view engine", "pug");
 app.get("/", (req, res) => {
-  res.render("index", { lat, lng, APP_KEY });
-  console.log(lat, lng);
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, () => {
